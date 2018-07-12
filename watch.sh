8fe3b62d74dadaa844f4f9528dd195eda930b691
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-07-12 19:29:26 -0400

    Fixed .d.ts file.

--- watch.sh
+++ watch.sh
@@ -1,3 +1,3 @@
 node node_modules/typescript/bin/tsc --watch -p tsconfig.d.ts.json &
-#node ./node_modules/run-when-changed/bin/run-when-changed.js --watch "./dist/p-info.d.ts" --exec "node ./node_modules/clean-typings-file ./dist/p-info.d.ts p-info" &
+node ./node_modules/run-when-changed/bin/run-when-changed.js --watch "./dist/p-info.d.ts" --exec "node ./node_modules/clean-typings-file ./dist/p-info.d.ts p-info" &
 node node_modules/webpack/bin/webpack --progress --watch --env.node --display-error-details
